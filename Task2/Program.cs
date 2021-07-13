6558278f82d14a9b453a2633d629db2e4d7cf9de
Dmitrij <kramkov@ncpi.gov.by>
2021-07-13 12:35:00 +0300

    Age: added functionality for set and get user age

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -11,7 +11,10 @@ namespace Task2
             Console.Write("Enter your name: ");
             user.Name = Console.ReadLine();
 
-            Console.WriteLine($"Hello {user.Name}!");
+            Console.Write("Enter your age: ");
+            user.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Hello {user.Name}\nYour age {user.Age}!");
 
             Console.ReadKey();
         }
