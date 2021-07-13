3e802dee4e4e950df371d2d880c86f4c803baa83
Cram <kramkovdd@gmail.com>
2021-07-13 20:58:13 +0300

    added functionality for task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Text.Json;
 
 namespace Task1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+
+            Console.WriteLine(jsonString);
+            Console.ReadKey();
         }
     }
 }
---
+++ Task1/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string Summary { get; set; }
+    }
+}
