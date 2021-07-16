a1024442a7d0b118d713b0de0dbc1419fd07b346
Dmitrij <kramkov@ncpi.gov.by>
2021-07-16 14:18:29 +0300

    added wrong class for SRP

---
+++ Task4/Solid(SRP)/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task4.Solid_SRP_
+{
+    public interface IUser
+    {
+        public IEnumerable<object> GetItems();
+
+        public int GetItemsCount();
+
+        public void AddItem(object item);
+
+        public void DeleteItem(int id);
+
+        public void PrintUser();
+
+        public void ShowUser();
+
+        public void Load();
+                          
+        public void Save();
+
+        public void Update();
+                            
+        public void Delete();
+    }
+}
