959b6b6928c96dc1bb75e278a8f940fd8ea70318
Dmitrij <kramkov@ncpi.gov.by>
2021-07-16 13:43:28 +0300

    Decorator: added example with decorator pattern

---
+++ Task4/Decorator/ConcreteComponents/PeperoniPizza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task4.Decorator.ConcreteComponents
+{
+    public class PeperoniPizza : Pizza
+    {
+        public PeperoniPizza() : base("Пицца пеперони")
+        {
+            Price = 20;
+        }
+    }
+}
---
+++ Task4/Decorator/ConcreteDecorators/OnionPizza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task4.Decorator.ConcreteDecorators
+{
+    class OnionPizza : PizzaDecorator
+    {
+        public OnionPizza(Pizza pizza) : base(pizza)
+        {
+            Name += " с луком";
+            Price += 1;
+        }
+    }
+}
